efe10d69ccc37e6751e7269e175e15bf1bf39ead
david <david.zinkiv@gmail.com>
2025-11-23 18:11:06 -0500

    added agent observability tool

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
 from typing import Union
 from fastapi import FastAPI
+from app.models.chat_models import UserChatRequest, ChatHistory, ChatMessage
+import uuid
+from phoenix.otel import register
+
+tracer_provider = register(project_name="API Agent", auto_instrument=True)
 
 app = FastAPI()
 
 
 @app.post("/chat")
-def new_chat():
-    return {"chat_id": "12345"}
+def new_chat(request: UserChatRequest):
+    user_info = {
+        "user_id": request.user_id,
+        "email": request.email,
+        "username": request.username,
+        "first_name": request.first_name,
+    }
+    return {"chat_id": uuid.uuid4()}
+
+
+@app.get("/chat/{chat_id}")
+def get_message_history(chat_id: int, request: ChatMessage):
+    return {"chat_id": chat_id, "messages": []}
 
 
 @app.post("/chat/{chat_id}/message")
-def message():
-    return {"chat_id": "12345"}
-
-
-@app.get("/chat/{chat_id}/message")
-def get_messages(chat_id: int, q: Union[str, None] = None):
-    return {"chat_id": chat_id, "q": q}
+def message(chat_id: int):
+    return {"message": "AI response"}
